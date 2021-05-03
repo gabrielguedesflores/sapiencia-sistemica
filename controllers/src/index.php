cb1da4b5b1b4376882751bf0f9f6e9ef8569fd90
Gabriel Guedes Flores <60441476+gabrielguedesflores@users.noreply.github.com>
2021-05-03 15:42:34 -0300

    hidden pass

--- controllers/src/index.php
+++ controllers/src/index.php
@@ -17,7 +17,7 @@ try {
 	$mail->Host = 'smtp.gmail.com';
 	$mail->SMTPAuth = true;
 	$mail->Username = 'sapienciasistemica@gmail.com';
-	$mail->Password = 'sapi3nciasist3mica!';
+	$mail->Password = ''; 
 	$mail->Port = 587;
 
 	$mail->setFrom('gabrielguedesflores@gmail.com');
